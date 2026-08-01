f5752528a3349fb9f8cb15371c05a67a4b924f3f
son090103 <144188086+son090103@users.noreply.github.com>
2026-08-02 01:20:49 +0700

    feat: implement revenue statistics API and add historical data seeding script

---
+++ src/controller/admin/statistics.controller.js
@@ -0,0 +1,26 @@
+const statisticsService = require("../../service/admin/statistics.service");
+
+module.exports.getDashboardStats = async (req, res) => {
+  try {
+    const { timeframe, startDate, endDate, year, month, week } = req.query;
+    const result = await statisticsService.getAdminDashboardStats({ timeframe, startDate, endDate, year, month, week });
+    return res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    console.error("Error in getDashboardStats controller:", error);
+    return res.status(500).json({ success: false, message: "Lỗi Server", error: error.message });
+  }
+};
+
+module.exports.getAdvancedStats = async (req, res) => {
+  try {
+    const result = await statisticsService.getAdvancedAnalysisStats();
+    if (!result) {
+      return res.status(404).json({ success: false, message: "Chưa có báo cáo phân tích nâng cao. Vui lòng chạy file Python trước." });
+    }
+    console.log("thành công")
+    return res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    console.error("Error in getAdvancedStats controller:", error);
+    return res.status(500).json({ success: false, message: "Lỗi Server", error: error.message });
+  }
+};
